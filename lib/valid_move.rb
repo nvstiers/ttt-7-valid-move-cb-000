def72f21045778256d466562f6f4228160f310d0
Nathaniel Stiers <nvstiers@gmail.com>
2019-04-04 15:20:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,23 @@
 # code your #valid_move? method here
-
+valid = nil
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board,index) == false
+    valid = true
+  elsif index.between?(0,8) && position_taken?(board,index) == true
+    valid = false
+  else
+    valid = false
+  end
+  valid
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+taken = nil
+def position_taken?(board, index)
+  if board[index] == " " || "" || nil
+    taken = false
+  else
+    taken = true
+  end
+  taken
+end
